efc4633c2e4f26aa15fc4ad9a0b70ed6d5e42068
Layesoda <diopabdoulaye08@gmail.com>
2022-06-30 07:31:52 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+# inclure  < stdio.h >
+
+/* *
+ *
+ * * principal - Point d'entrée
+ *
+ * *
+ *
+ * * Retour : Toujours 0 (Succès)
+ *
+ *  */
+
+int  principal ( vide )
+
+{
+
+		puts ( " \" Programmer, c'est comme construire un puzzle multilingue " );
+
+			retour ( 0 );
+}
